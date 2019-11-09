82b56408bc4bfbc4c1b95aff8dd3cf51e312d834
Thomas Heinemann <data1701-e@web.de>
2019-11-09 13:34:39 +0100

    Add another test

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 from src.calculator import Stock, calculate
 
 class CalculatorTest(unittest.TestCase):
@@ -10,14 +11,40 @@ class CalculatorTest(unittest.TestCase):
         ]
         names = ['Jane']
 
-        calendar = calculate(stock, names)
+        calendars = calculate(stock, names)
 
-        self.assertEquals(len(calendar), 1)
-        self.assertEquals(len(calendar['Jane']), 24)
+        self.assertEqual(len(calendars), 1)
+        self.assertEqual(len(calendars['Jane']), 24)
 
-        self.assertEquals(list(calendar['Jane'].values()).count('chocolate'), 7)
-        self.assertEquals(list(calendar['Jane'].values()).count('sweets'), 8)
-        self.assertEquals(list(calendar['Jane'].values()).count('coal'), 9)
+        calendar = calendars['Jane']
+        self.assertEqual(list(calendar.values()).count('chocolate'), 7)
+        self.assertEqual(list(calendar.values()).count('sweets'), 8)
+        self.assertEqual(list(calendar.values()).count('coal'), 9)
+
+    @patch('random.shuffle')
+    def test_randomizes_order_of_presents(self, shuffle):
+        stock = [
+            Stock('wine', 24),
+            Stock('beer', 24),
+        ]
+
+        randomized_stock = ['wine', 'beer'] * 24
+        expected_stock = ['wine'] * 24 + ['beer'] * 24
+        def shuffle_effect(input):
+            self.assertEqual(input, expected_stock)
+            for n, _ in enumerate(input):
+                input[n] = randomized_stock[n]
+        shuffle.side_effect = shuffle_effect
+
+        names = ['Jane', 'John']
+        calendars = calculate(stock, names)
+
+        janesCalendar = calendars['Jane']
+        johnsCalendar = calendars['John']
+        self.assertEqual(list(janesCalendar.values()), ['wine'] * 24)
+        self.assertEqual(list(johnsCalendar.values()), ['beer'] * 24)
+
+        shuffle.assert_called()
 
     def test_raise_value_error_if_not_enough_stock_is_present(self):
         stock = [
@@ -28,4 +55,4 @@ class CalculatorTest(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             calculate(stock, names)
 
-        self.assertEquals(str(context.exception), 'Not enough presents for everyone :(')
+        self.assertEqual(str(context.exception), 'Not enough presents for everyone :(')
