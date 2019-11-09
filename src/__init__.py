c88ae5f0d2d041cccb1f35d5e637e2967fc35b1f
Thomas Heinemann <data1701-e@web.de>
2019-11-09 15:46:21 +0100

    Add CSV output of advent calendar

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,12 @@
-from .cli import parse_arguments
+from .cli import parse_arguments, print_calendars
+from .calculator import calculate
+from .csvParser import parseCsv
 import sys
 
 def main(args = sys.argv[1:]):
-    parse_arguments(args)
+    config = parse_arguments(args)
+    stock = parseCsv(config.stock)
+
+    results = calculate(stock, config.calendar_names)
+
+    print_calendars(results)
--- src/cli.py
+++ src/cli.py
@@ -1,9 +1,22 @@
 import argparse
-
+import sys
+import csv
 
 def parse_arguments(args):
     parser = argparse.ArgumentParser(description = 'Create the presents of advent calendars based on stock')
     parser.add_argument('stock', metavar='STOCk', help='A file which contains the stock of presents in CSV format')
     parser.add_argument('calendar_names', metavar='NAME', nargs='+', help='The names of the calendars, e.g. names of the children')
 
-    parser.parse_args(args)
+    return parser.parse_args(args)
+
+def print_calendars(calendars, output=sys.stdout):
+    names = list(calendars.keys())
+    days = list(calendars[names[0]].keys())
+    days.sort()
+
+    writer = csv.writer(output)
+    writer.writerow(['Day', *names])
+
+    for day in days:
+        row = [day, *[calendars[name][day] for name in names]]
+        writer.writerow(row)
